2cde998c5aef6c6174fb63b1ec036b2f089e54c2
david simmons <davidl_86@hotmail.com>
2025-05-26 18:53:15 -0500

    initial commit

---
+++ myFunctions.py
@@ -0,0 +1,34 @@
+'''
+This is referred to as a module and is imported into the main function
+via the import command. 
+'''
+
+'''
+var1 = openFileRead()
+In this module the filepath is set as the default and is not required to be
+passed to the module from within the main program. In order to access the 
+list information the user must assign the results to var1
+'''
+FILEPATH = "/Users/david/Desktop/Python Course/"
+
+def openFileRead(file_path=FILEPATH):
+    with open(file_path + 'tasks.txt', 'r') as read_file:
+        local_tasks = read_file.readlines()
+    return(local_tasks)
+
+
+'''
+var1 = openFileWrite(tasks)
+In this module the filepath is set as the default and a 'task' list is required 
+to be passed to the module from within the main program. There is no data to be
+returned so the function acts like a procedure
+'''
+
+def openFileWrite(wrt_tasks, file_path=FILEPATH):
+    with open(file_path + 'tasks.txt', 'w') as write_file:
+        write_file.writelines(wrt_tasks)
+
+
+if __name__ == '__main__':
+    msg = openFileRead()
+    print(msg)
---
+++ webgui.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import myFunctions as functions
+
+# Global Variables
+myTasks = functions.openFileRead()
+
+
+def addNewTask():
+    """ Function/Method explanation goes here """
+    myTask = st.session_state["new_task"]
+    print(myTask)
+    myTasks.append(myTask) + "\n"
+    print(myTasks)
+    functions.openFileWrite(myTasks)
+
+
+def taskPage():
+    """ Function/Method explanation goes here """
+    st.title("Daily Task List Web App")
+    st.subheader("Task List App")
+    st.write("Simple Task List app to improve your productivity.")
+
+    for index, tasks in enumerate(myTasks):
+        checkbox = st.checkbox(tasks, key=tasks)
+        if checkbox:
+            myTasks.pop(index)
+            functions.openFileWrite(myTasks)
+            del st.session_state[tasks]
+            st.rerun()
+
+    st.text_input(label="Add a new Task Item", 
+                  label_visibility="hidden", 
+                  placeholder="Enter a new task",
+                  on_change=addNewTask, 
+                  key='new_task')
+    
+    st.session_state
+
+if __name__ == "__main__":
+    taskPage()
